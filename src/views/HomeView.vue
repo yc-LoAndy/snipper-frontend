2ba3bd6d05bbbfb5d616ee61536838f20e92c95f
YUAN CHUN, LO <andyyuanchun@gmail.com>
2025-03-17 18:15:53 +0800

    feat: main page header

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2 class="main-text">Welcome to Snipper!</h2>
+    <p class="main-text">Start managing your code snippets.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
