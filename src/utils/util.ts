44618b2880941a0a7cc66cda4f551ebb13fd5719
yc-LoAndy <andyyuanchun@gmail.com>
2025-03-25 22:01:26 +0800

    feat: more util function

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,16 +1,21 @@
-import type { TreeNode } from 'primevue/treenode';
+export function getFileExtension(filePath: string) {
+	const fileName = filePath.split('/').pop();
+	if (!fileName)
+		return '';
+	const parts = fileName.split('.');
+	if (parts.length <= 1) return '';
+	const extension = parts.pop()!.toLowerCase();
+	return extension;
+}
 
-export function getTreeNodeByKey(nodes: TreeNode[], key: string): TreeNode | undefined {
-    for (const node of nodes) {
-        if (node.key === key) {
-            return node;
-        }
-        if (node.children) {
-            const found = getTreeNodeByKey(node.children, key);
-            if (found) {
-                return found;
-            }
-        }
-    }
-    return undefined;
+export function trimAny(str: string, chars: string[]) {
+    let start = 0, end = str.length;
+
+    while(start < end && chars.indexOf(str[start]) >= 0)
+        ++start;
+
+    while(end > start && chars.indexOf(str[end - 1]) >= 0)
+        --end;
+
+    return (start > 0 || end < str.length) ? str.substring(start, end) : str;
 }
