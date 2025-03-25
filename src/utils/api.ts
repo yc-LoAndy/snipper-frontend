ca852a563661149ac109c530df4a0a19f2ae3312
yc-LoAndy <andyyuanchun@gmail.com>
2025-03-25 15:10:19 +0800

    fix: recursive handling 401 error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,51 +1,59 @@
 import axios from 'axios';
 import { ref } from 'vue';
-import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import createAuthRefreshInterceptor, { type AxiosAuthRefreshRequestConfig } from 'axios-auth-refresh';
 
 const api = axios.create({ baseURL: '/api' });
 export const apiError = ref<string | null>(null);
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const refreshAccessToken = async (failedRequest: any) => {
-	const response = await api.post('/token', {}, { withCredentials: true });
-	if (response.status === 401) {
-		localStorage.removeItem('accessToken');
-		window.location.href = '/';
-		return Promise.reject(failedRequest);
-	}
-	const { accessToken } = response.data;
-	localStorage.setItem('accessToken', accessToken);
-	failedRequest.response.config.headers['Authorization'] = `Bearer ${accessToken}`;
-	return Promise.resolve();
+    try {
+        const response = await axios.post(
+            '/api/token', {}, {
+                withCredentials: true, skipAuthRefresh: true
+            } as AxiosAuthRefreshRequestConfig
+        );
+        const { accessToken } = response.data;
+
+        localStorage.setItem('accessToken', accessToken);
+        failedRequest.response.config.headers['Authorization'] = `Bearer ${accessToken}`;
+        return Promise.resolve(failedRequest);
+    } catch (error: any) {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('accessToken');
+            error.response.data.message = 'User logged-out';
+        }
+        return Promise.reject(error);
+    }
 };
 
 // Request interceptors
 // Add the access-token to the Authorization header
 api.interceptors.request.use(
-	(config) => {
-		const token = localStorage.getItem('accessToken');
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-		return config;
-	}
+    (config) => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }
 );
 
 // Response interceptors
 createAuthRefreshInterceptor(api, refreshAccessToken);
 
 api.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		if (error.response) {
-			const status = error.response.status;
-			const requestId = error.response.headers['x-request-id'] ?? 'Unknown';
-			const errMessage = error.response.data.message || 'An error occurred';
-			apiError.value = `Error ${status}: ${errMessage} (Request ID: ${requestId})}`;
-		} else {
-			apiError.value = 'Network error';
-		}
-		return Promise.reject(error);
-	}
+    (response) => response,
+    async (error) => {
+        if (error.response) {
+            const status = error.response.status;
+            const requestId = error.response.headers['x-request-id'] ?? 'Unknown';
+            const errMessage = error.response.data.message || 'An error occurred';
+            apiError.value = `Error ${status}: ${errMessage} (Request ID: ${requestId})}`;
+        } else {
+            apiError.value = 'Network error';
+        }
+        return Promise.reject(error);
+    }
 );
 
 
